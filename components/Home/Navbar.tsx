a01b808a141bfad48335b849130c4b71fd350a77
= <=>
2022-06-23 10:51:56 +0700

    update navbar homepage

--- components/Home/Navbar.tsx
+++ components/Home/Navbar.tsx
@@ -4,8 +4,9 @@ import Popup from 'reactjs-popup';
 export default function Navbar() {
 
   let Links = [
-    { class: 'link', name: 'About', href: '/'},
+    { class: 'link', name: 'Home', href: '/'},
     { class: 'link', name: 'Project', href: '/project'},
+    { class: 'link', name: 'Certificate', href: '/certificate'},
   ]
 
   return (
@@ -14,8 +15,10 @@ export default function Navbar() {
             <h1 className="text-[30px] mt-[35px] mb:text-[40px]">Union</h1>
         </div>
         <div className="hidden ml-[260px] text-[25px] gap-16 mt-[45px] md:flex" data-aos="fade-left" data-aos-duration="2000">
-            <div className="border-none duration-500 text-slate-500 hover:text-slate-900 hover:border-b-4 hover:h-9 hover:border-slate-900 hover:border-solid"><Link href='/'>About</Link></div>
-            <div className="border-none duration-500 text-slate-500 hover:text-slate-900 hover:border-b-4 hover:h-9 hover:border-slate-900 hover:border-solid"><Link href='/project'>Project</Link></div>
+
+            {Links.map((link) => (
+              <div key={link.name} className="border-none duration-500 text-slate-500 hover:text-slate-900 hover:border-b-4 hover:h-9 hover:border-slate-900 hover:border-solid"><Link href={link.href}>{link.name}</Link></div>
+            ))}
             <div className="border-none duration-500 text-slate-500 hover:text-slate-900 hover:border-b-4 hover:h-9 hover:border-slate-900 hover:border-solid">
               <Popup trigger={<button>Blog</button>} position="bottom center">
                   <h1 className="bg-blue-400 py-2 px-4 font-bold font-poppins">Coming Soon...</h1>
