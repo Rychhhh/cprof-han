292fcb37a5a4750567143c8a0e46ecb48a81f3bb
= <=>
2022-06-23 10:54:07 +0700

    update navbar on project page

--- components/project/Navbar.tsx
+++ components/project/Navbar.tsx
@@ -4,8 +4,9 @@ import Popup from 'reactjs-popup';
 export default function Navbar() {
 
   let Links = [
-    { class: 'link', name: 'About', href: '/'},
+    { class: 'link', name: 'Home', href: '/'},
     { class: 'link', name: 'Project', href: '/project'},
+    { class: 'link', name: 'Certificate', href: '/certificate'},
   ]
 
   return (
@@ -15,20 +16,23 @@ export default function Navbar() {
       </div>
       
       <div className="hidden md:flex lg:space-x-52" data-aos="fade-left" data-aos-duration="1500"> 
-          <div className="link ml-5 pt-1 space-x-2  md:space-x-10 lg:space-x-16 md:text-xl">
-            <span className="underline-offset-4 hover:underline duration-75">
-              <Link href={'/'}> About </Link>
-            </span>
-            <span className="underline-offset-4 hover:underline duration-75">
-              <Link href={'/project'}> Project </Link> 
-            </span>
-            <span className="underline-offset-4 hover:underline duration-75">
-              <Popup trigger={<button className="font-bold underline-offset-4 hover:underline">Blog</button>} position="bottom center">
-                <div className="py-2 px-4 bg-orange-200 font-semibold font-whytee">
-                  <h1>Segera Dibuat</h1>
-                </div>
-              </Popup>
-            </span>
+          <div className=" ml-5 pt-1 space-x-2  md:space-x-10 lg:space-x-16 md:text-xl">
+             <span className="link underline-offset-4 hover:underline duration-75">
+                <Link href={'/'}> Home </Link>
+              </span>
+              <span className="link underline-offset-4 hover:underline duration-75">
+                <Link href={'/project'}> Project </Link> 
+              </span>
+              <span className="link underline-offset-4 hover:underline duration-75">
+                <Link href={'/certificate'}> Certificate </Link> 
+              </span>
+              <span className="link underline-offset-4 hover:underline duration-75">
+                <Popup trigger={<button className="font-bold underline-offset-4 hover:underline">Blog</button>} position="bottom center">
+                  <div className="py-2 px-4 bg-orange-200 font-semibold font-whytee">
+                    <h1>Segera Dibuat</h1>
+                  </div>
+                </Popup>
+              </span>`
            
           </div>
 
