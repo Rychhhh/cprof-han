64dfeec002fcb4a1f20fc4fd5a823cf978387407
= <=>
2022-06-23 10:55:44 +0700

    configuration css global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react'
 import AOS from 'aos';
 import 'aos/dist/aos.css'; // You can also use <link> for styles
+import '../styles/keyframes.css'
 
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
